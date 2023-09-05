c2b7ef00076059a32cc652eb9cf784c0fc553c82
Nikita Mashchenko <mashchenko1@icloud.com>
2023-09-05 13:01:14 -0500

    fix: folder naming, logger config

--- account-review/application.py
+++ account-review/application.py
@@ -1,13 +1,31 @@
 from flask import Flask
 from api.auth import authRequest
 from db import init_db
+from logging.config import dictConfig
+
 
 def create_app() -> Flask:
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': "%(levelname)s:\t %(asctime)s - [%(name)s] %(filename)s::%(funcName)s - %(message)s",
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
     app = Flask(__name__)
     app.config.from_object('config')
 
     if 'vantage' not in app.config["SKU"]:
-        raise Exception("Only VANTAGE SKU allowed for this app, please change in config.py")
+        raise Exception(
+            "Only VANTAGE SKU allowed for this app, please change in config.py")
 
     # Get access token
     accessToken = authRequest(app)
